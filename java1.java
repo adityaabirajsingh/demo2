963b1e03c57a67c20f1d213dd75904463a7916fc
adityaabirajsingh <adityaabirajsingh@gmail.com>
2026-01-30 10:10:22 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -1,17 +1,26 @@
-nums = [2, 4, 7, 10, 15, 20]
-print("Example array:",nums)
-a=int(input("Enter number to search in the example array : "))
-print("position is",binary_search(nums, a)+1) 
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
 
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // not found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 7, 10, 15, 20};
+        System.out.println(binarySearch(nums, 10)); // Output: 3
+    }
+}
+
